f4aa2bf4ad35c72e131cd4f84c4af665b795cddd
Chumarin Rafael <rchumarin@gmail.com>
2015-07-22 22:35:47 +0400

    Получение фильтра вынесено из цикла. Применены отступы, скобки.

--- ICL2 JAVA IO/src/Factory.java
+++ ICL2 JAVA IO/src/Factory.java
@@ -1,11 +1,10 @@
-
 public class Factory {
 	public Filter getFilter(int num) {  
 		if(num=='1')
 			return new MyFilter1();
 		else if(num=='2')
 			return new MyFilter2();
-		return null;
+		else return null;
 	}
 }
 
--- ICL2 JAVA IO/src/Main.java
+++ ICL2 JAVA IO/src/Main.java
@@ -1,36 +1,32 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		String line, fileNotFound;
+	public static void main(String[] args) throws IOException {
+		String line, fileNotFound; 
 		int num;			 
-/*
+		/*
 		out.print("Применение фильтров. Укажите путь к файлу: ");
 		BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
 		File file = new File(reader.readLine());
-		fileNotFound = String.format(file.exists() ? "Абсолютный путь: " + file.getAbsolutePath(): "Файл не найден"); 
-		out.println(fileNotFound);
-*/
+		 */
 		File file = new File("./first.txt");
+		if (!file.exists()) {
+			System.out.println("Файл не найден"); 
+			return;
+		}
 		System.out.println("Фильтр №1 - на экране печатаются только те строки файла где нету слова Путин");
 		System.out.println("Фильтр №2 - на экране печатаются только те строки файла где есть слова Путин, но нет слома Медведев");
 		System.out.print("Введите номер фильтра: ");
-		num = System.in.read();
-		
+		num = System.in.read();	
 		try (BufferedReader filer = new BufferedReader(new FileReader(file))) {
+			Factory factory = new Factory();
+			Filter filter = factory.getFilter(num);
 			do {
 				line = filer.readLine();
-				Factory factory = new Factory();
-				Filter filter = factory.getFilter(num); 
-				try {
-					if (filter.accept(line)) System.out.println(line);
-				}
-				catch(NullPointerException exc) {
-					System.out.println("Не правильно указан номер фильтра");
-					return;
-				}
-				
-			}
+				if (filter.accept(line)) 
+					System.out.println(line);
+			}		
 			while(line != null);
 		}
 	}
